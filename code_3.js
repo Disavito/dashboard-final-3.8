ad92ac01c2e8441ce71979681b6ff7918ab4de83
Disavito <dsvt98@gmail.com>
2026-03-04 01:56:56 -0500

    Delete code_3.js

--- code_3.js
+++
@@ -1,5 +0,0 @@
-Error occurred at /jornada
-Port: 5173
-
-Stack trace:
-SyntaxError: The requested module '/src/lib/api/jornadaApi.ts?t=1772486186157' does not provide an export named 'calculateWorkedMinutesForJornada'
